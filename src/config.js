3df37a55a17632f152adb1ea1071b678a426f891
GabrielCode <bielaugusto8@hotmail.com>
2024-06-17 22:08:12 -0300

    Mudanças

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:5000/api/v1';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'https://emergcare-backend.onrender.com/api/v1';
 export const token = localStorage.getItem("token")
+
